ee05578456384d710407620378f0b4d196e4f895
Adam <alumm07@gmail.com>
2016-01-23 18:30:17 -0500

    incorrect class name

--- src/reading_with_exceptions/Printstream_Scanner_Examples.java
+++
@@ -1,5 +0,0 @@
-package reading_with_exceptions;
-
-public class Printstream_Scanner_Examples {
-	
-}
---
+++ src/reading_with_exceptions/Reading_with_Exceptions.java
@@ -0,0 +1,9 @@
+package reading_with_exceptions;
+
+public class Reading_with_Exceptions {
+
+	public static void main(String[] args) {
+
+	}
+
+}
