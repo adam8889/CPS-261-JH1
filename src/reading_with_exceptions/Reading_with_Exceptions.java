edaacaf413a03154970ef16c06cbd2db317749da
Adam <alumm07@gmail.com>
2016-01-23 20:08:14 -0500

    Begin writing process method

--- src/reading_with_exceptions/Reading_with_Exceptions.java
+++ src/reading_with_exceptions/Reading_with_Exceptions.java
@@ -1,9 +1,33 @@
 package reading_with_exceptions;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class Reading_with_Exceptions {
+	
+	public void process(String inputFilename) {
+		
+		try {
+			FileInputStream fi  = new FileInputStream(inputFilename);
+			Scanner filescanner = new Scanner(fi);
+		}
+		catch(FileNotFoundException e) {
+			
+		}
+	}
+	
+	private void copyNumbers(Scanner scan, PrintStream ps, int numIntsToRead) {
+		
+	}
+	
+	private void printToScreen(String filename) {
+		
+	}
 
 	public static void main(String[] args) {
-
+		
 	}
 
 }
